74d7022f5ad4438a996e7eb7ccde641b3a6c7483
Karl Obermiller <karl.obermiller@biosensics.com>
2021-08-04 15:25:28 -0400

    Add in-page feedback on whether image load failed

--- src/renderer.ts
+++ src/renderer.ts
@@ -34,15 +34,24 @@ console.log('👋 This message is being logged by "renderer.js", included via we
 
 const directoryButton: HTMLButtonElement = document.querySelector("#openDirectoryButton");
 const directoryLabel: HTMLElement = document.querySelector("#directoryLabel");
+const resultsEl: HTMLElement = document.querySelector("#results");
 
 directoryButton.addEventListener("click", () => {
     const result = ipcRenderer.sendSync("showOpenDialog", {properties: ["openDirectory"]});
     if (result.canceled) return;
+    resultsEl.innerText = "";
+    resultsEl.style.color = undefined;
     const directory = result.filePaths[0];
 
     directoryLabel.innerText = directory;
     loadImages(directory).then((image) => {
         console.log(image);
+        resultsEl.style.color = "forestgreen"
+        resultsEl.innerText = "Image Loaded Successfully!";
+    }).catch(err => {
+        console.error(err);
+        resultsEl.style.color = "darkred"
+        resultsEl.innerText = `Error encountered:\n${err.stack || JSON.stringify(err)}`
     });
 })
 
